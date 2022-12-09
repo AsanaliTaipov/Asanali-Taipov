f8d207e06456eaab9b772e17c9802ce5621a7661
AsanaliTaipov <120211795+AsanaliTaipov@users.noreply.github.com>
2022-12-10 01:18:44 +0600

    Add files via upload

---
+++ Questions.java
@@ -0,0 +1,11 @@
+public class Questions {
+    public static void main(String[] args) {
+        System.out.println("Name: Assanali");
+        System.out.println("Surname: Taipov");
+        System.out.println("Id: 220103162");
+        System.out.println("Hobby: football,watch series,");
+        System.out.println("How much do you like ICT(from 1-10): 10");
+        System.out.println("rate this task from 1 to 10: 5");
+        System.out.println("My favourite color?: red");
+    }
+}
